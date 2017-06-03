2deaa2aae0b9dfb2b0f239695baa4c53fa3ac859
Alban <alban.menager@gmail.com>
2017-06-03 13:52:56 +0200

    aaaa

--- start.js
+++ start.js
@@ -37,15 +37,15 @@ if (cluster.isMaster) {
   }
 } else {
   require('./app.js');
+  
+  const Discord = require('discord.js');
+  const bot = new Discord.Client();
+
+  const Team = require('./commands/team');
+
+  bot.login("MzIwMjU1NTI3MDE4MDM3MjQ4.DBM8vg.rCFTw5J5t_jcjILtjtTwoJw1x5E");
+
+  bot.on("message", function (message){
+      Team.parse(message);
+  })
 }
-
-const Discord = require('discord.js');
-const bot = new Discord.Client();
-
-const Team = require('./commands/team');
-
-bot.login("MzIwMjU1NTI3MDE4MDM3MjQ4.DBM8vg.rCFTw5J5t_jcjILtjtTwoJw1x5E");
-
-bot.on("message", function (message){
-    Team.parse(message);
-})
