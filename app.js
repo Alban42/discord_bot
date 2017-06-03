6831195605310a86f42626aef8074a842b48de44
Alban <alban.menager@gmail.com>
2017-06-03 16:37:57 +0200

    change

--- app.js
+++ app.js
@@ -10,6 +10,18 @@ let server = http.createServer(function (req, res) {
   if (url == '/') {
     url += 'index.html';
   }
+  
+  const Discord = require('discord.js');
+  const bot = new Discord.Client();
+
+  const Team = require('./commands/team');
+
+  bot.login("MzIwMjU1NTI3MDE4MDM3MjQ4.DBM8vg.rCFTw5J5t_jcjILtjtTwoJw1x5E");
+
+  bot.on("message", function (message){
+      Team.parse(message);
+  });
+
 
   // IMPORTANT: Your application HAS to respond to GET /health with status 200
   //            for OpenShift health monitoring
@@ -44,13 +56,3 @@ server.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function () {
   console.log(`Application worker ${process.pid} started...`);
 });
 
-const Discord = require('discord.js');
-const bot = new Discord.Client();
-
-const Team = require('./commands/team');
-
-bot.login("MzIwMjU1NTI3MDE4MDM3MjQ4.DBM8vg.rCFTw5J5t_jcjILtjtTwoJw1x5E");
-
-bot.on("message", function (message){
-    Team.parse(message);
-});
