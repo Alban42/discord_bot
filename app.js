7870b8f1014daabb5fe64548d6a6fb05fd1b969e
Alban <alban.menager@gmail.com>
2017-06-03 14:01:39 +0200

    Change place

--- app.js
+++ app.js
@@ -43,3 +43,14 @@ let server = http.createServer(function (req, res) {
 server.listen(env.NODE_PORT || 3000, env.NODE_IP || 'localhost', function () {
   console.log(`Application worker ${process.pid} started...`);
 });
+
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const Team = require('./commands/team');
+
+bot.login("MzIwMjU1NTI3MDE4MDM3MjQ4.DBM8vg.rCFTw5J5t_jcjILtjtTwoJw1x5E");
+
+bot.on("message", function (message){
+    Team.parse(message);
+});
