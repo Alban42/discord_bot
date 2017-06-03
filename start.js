e1d4d0834fa8230ed39eca92b54b6ddcaf84a46f
Alban <alban.menager@gmail.com>
2017-06-03 13:19:23 +0200

    test

--- start.js
+++ start.js
@@ -38,3 +38,14 @@ if (cluster.isMaster) {
 } else {
   require('./app.js');
 }
+
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const Team = require('./commands/team');
+
+bot.login("MzIwMjU1NTI3MDE4MDM3MjQ4.DBM8vg.rCFTw5J5t_jcjILtjtTwoJw1x5E");
+
+bot.on("message", function (message){
+    Team.parse(message);
+})
